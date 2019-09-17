c879a610168bf0ccf9fac42ea00eaa9581764d9f
Stephanie Jordan <sjordan@middlebury.edu>
2019-09-17 14:29:18 -0400

    first commit

--- color-picker.js
+++ color-picker.js
@@ -13,22 +13,35 @@ returns all changes via a callback. The callback is passed an object of the form
 */
 const createSlider = function createSlider(color, initialValue, callback) {
   // create a div to hold all of the slider elements
- 
+  const slider = document.createElement('div');
+  slider.className = `${color}-slider`;
 
   // create a div to hold the color name and add it to the slider
-
+  const label = document.createElement('div');
+  label.className = 'color-label';
+  label.innerHTML = `${color}:`;
+  slider.appendChild(label);
 
   // create the range input and add it to the slider
-  
+  const range = document.createElement('input');
+  range.type = 'range';
+  range.min = 0;
+  range.max = 255;
+  range.value = initialValue;
+  slider.appendChild(range);
 
   // create the readout to display the current value and add it to the slider
-  
-
+  const readout = document.createElement('span');
+  readout.innerHTML = initialValue;
+  slider.appendChild(readout);
   // set the range input's oninput function to update the readout and call the callback
-  
+  range.oninput = () => {
+    readout.innerHTML = range.value;
+    callback({ [color]: parseInt(range.value, 10) });
+  };
 
   // return the slider
-
+  return slider;
 };
 
 
@@ -46,30 +59,43 @@ whenever the value of the color changes.
 */
 const createColorPicker = function createColorPicker(initialValue, callback) { // eslint-disable-line no-unused-vars, max-len
   // create a div to hold the picker
-  
+  const picker = document.createElement('div');
+  picker.className = 'color-picker';
 
   // create a div with the class 'color-swatch' to provide the colored rectangle
   // and add it to the picker
-  
+  const colorBox = document.createElement('div');
+  colorBox.className = 'color-swatch';
+  picker.appendChild(colorBox);
 
   // create a local variable to hold the current color and initialize it with initialValue
-  
+  let currentColor = initialValue;
 
   // create an update function that takes in an object of the form {color: value}
   // this should:
   // - merge the change into the the current color
   // - set the background color of the swatch
   // - call the callback with the current color
-  
+  const update = function (newColor) {
+    currentColor = { ...currentColor, ...newColor };
+    const { red, green, blue } = currentColor;
+
+    colorBox.style.background = `rgb(${red}, ${green}, ${blue})`;
+    callback(currentColor);
+  };
 
   // add sliders for each color channel
-  
+  Object.keys(currentColor).forEach((color) => {
+    // initialize slider in here
+    const slider = createSlider(color, currentColor[color], update);
+    picker.appendChild(slider);
+  });
 
   // call update() to initialize to the correct value
-  
+  update();
 
   // return the picker
-  
+  return picker;
 };
 
 try {
